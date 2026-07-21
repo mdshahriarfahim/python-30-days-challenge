56ef0cc3850b639669bc30cd9016415728f9811b
mdshahriarfahim <msfahim71291@gmail.com>
2026-07-21 22:13:52 +0600

    Added Day 01 Python first program

---
+++ 1-My_First_program.py
@@ -0,0 +1,8 @@
+print("Hello, World!")
+
+
+print("Welcome to my first Python program!")
+print("I am learning how to write and run python code")
+print("My name is Fahim and i am excited to learn more about programming ")
+
+print("I finished my first day program successfully")
